5e7cb112ac18001981be9fd70d26e6700b465cef
Sidmarek <chodec.marek@gmail.com>
2014-08-14 22:30:12 +0200

    #2 Fixing that bug with bad rendering

--- Sinatra/app.rb
+++ Sinatra/app.rb
@@ -1,11 +1,12 @@
 require "rubygems"
 require "sinatra"
+class NoNameProject < Sinatra::Application
 get '/hello/:name/:city/' do
  "Hello #{params[:name]} from #{params[:city]}!"
 end
 get '/form' do
  erb :secret
 end
-post '/form' do
+post '/form/result' do
  "Thats yours data:<br>'#{params[:name]}'<br>'#{params[:username]}'<br> Here is length of your password:<br>#{params[:password].length}"
 end
